3cf0ef8e486336f617f9219a54e4c15cadc80fa1
Prakhar Sharma <sharma.prakhar1@outlook.com>
2022-09-07 16:26:50 +0530

    savechange7-9

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,30 +1,64 @@
-//set initial count
+// //set initial count
+// let count = 0;
+
+// //select value and buttons
+// const value = document.querySelector("#value");
+// const btns = document.querySelectorAll(".btn");
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//         const styles = e.currentTarget.classList;
+//         if(styles.contains("decrease")){
+//             count--;
+//         }
+//         else if (styles.contains("increase")) {
+//             count++;
+//         } else {
+//             count = 0;
+//         }
+//         if (count > 0) {
+//             value.style.color ="green";
+//         }
+//         if (count < 0) {
+//             value.style.color ="red";
+//         }
+//         if (count === 0) {
+//             value.style.color ="black";
+//         }
+//         value.textContent = count;
+//     });
+// });
+
+// ----------------------------------------------------
+
 let count = 0;
 
-//select value and buttons
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value")
+const btns = document.querySelectorAll(".btn")
 
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        const styles = e.currentTarget.classList;
-        if(styles.contains("decrease")){
-            count--;
-        }
-        else if (styles.contains("increase")) {
-            count++;
-        } else {
-            count = 0;
-        }
-        if (count > 0) {
-            value.style.color ="green";
-        }
-        if (count < 0) {
-            value.style.color ="red";
-        }
-        if (count === 0) {
-            value.style.color ="black";
-        }
-        value.textContent = count;
-    });
-});
+btns.forEach (function (btn){
+    btn.addEventListener("click", function (e){
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+        count--;
+    }
+    else if (styles.contains("increase")) {
+        count++;
+    }
+    else{
+        count = 0
+    }
+
+    if (count>0) {
+        value.style.color = "green"
+    }
+    if (count<0) {
+        value.style.color = "red"
+    }
+    if (count===0) {
+        value.style.color = "black"
+    }
+    value.textContent = count
+
+    })
+})
